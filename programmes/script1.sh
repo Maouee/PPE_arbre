cc51e3d102d00094542e8e17f69a7deeb235add7
lucile bessac <lucilebessac@free.fr>
2023-11-22 15:51:58 +0100

    Lucile : création des tableaux

--- programmes/script1.sh
+++ programmes/script1.sh
@@ -9,15 +9,51 @@ then
 	exit
 fi
 
+echo "<html>
+        <head>
+            <link rel="stylesheet" href="../../Css/main.css">
+        </head>
+        <body>
+            <div class ="container">
+            <table id="table">" > ../tableaux/${langue}/tableau_${langue}.html
+
+echo "<tr>
+    <th>ligne</th>
+    <th>URL</th>
+    <th>code</th>
+    <th>encodage</th>
+    <th>aspirations</th>
+    <th>dumps</th>
+    </tr>" >> ../tableaux/${langue}/tableau_${langue}.html
+lineno=1
 
 while read -r URL
 do
+    ## EXTRACTION
     #récupère le contenu textuel et l'envoie vers un fichier .txt
     lynx -dump -nolist $URL > ../extractions/${langue}/extraction${compteur}.txt
     
     #récupère la valeur de l'encodage et l'écrit à la fin du fichier
     curl -L -I -s $URL | egrep -o "charset.+\b" | tail -1 | tr -d "\r\d" | tr -d "charset=" >> ../extractions/${langue}/extraction${compteur}.txt
 
+    ## TABLEAU
+
+    response=$(curl -s -I -L -w "%{http_code}" -o /dev/null $URL)
+    encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
+    echo "<tr>
+    <td>$lineno</td>
+    <td><a href=$URL>URL</a></td>
+    <td>$response</td>
+    <td>$encoding</td>
+    <td>
+    <a href="../aspirations/${langue}/aspiration${compteur}.html">aspiration</a>
+    </td>
+    <td>
+    <a href="../extractions/${langue}/extraction${compteur}.txt">extraction</a></td>
+    </tr>" >> ../tableaux/${langue}/tableau_${langue}.html
+    lineno=$(expr $lineno + 1)
     #incrémente le compteur de 1 avant de passer au fichier suivant
     ((compteur++))
 done < $file
+
+echo "</table></div></body></html>" >> ../tableaux/${langue}/tableau_${langue}.html
