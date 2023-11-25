f5eac55e5b9ee51cdf44424efe45b4d8e487f68a
lucile bessac <lucilebessac@free.fr>
2023-11-25 17:12:16 +0100

    Lucile : correction scripts

--- programmes/contexte/italien.sh
+++ programmes/contexte/italien.sh
@@ -1,3 +1,3 @@
 langue=$1
 compteur=$2
-egrep "alber(o|i)" num -2 ../extractions/${langue}/extraction${compteur}.txt > ../../contextes/${langue}/contexte${compteur}.txt
+egrep "alber(o|i)" -C 2 ../extractions/${langue}/extraction${compteur}.txt > ../contextes/${langue}/contexte${compteur}.txt
--- programmes/contexte/russe.sh
+++ programmes/contexte/russe.sh
@@ -1,3 +1,3 @@
 langue=$1
 compteur=$2
-cat $file | tr "[A-Z]" "[a-z]"| egrep -C 2 "(дерев[ауе]|деревом?|деревьях?|деревьями?|деревьев)" ../extractions/${langue}/extraction${compteur}.txt > ../contextes/${langue}/contexte${compteur}.txt
+egrep "(дерев[ауе]|деревом?|деревьях?|деревьями?|деревьев)" -C 2 ../extractions/${langue}/extraction${compteur}.txt > ../contextes/${langue}/contexte${compteur}.txt
