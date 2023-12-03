ca35c81f41d7d5aaa5e0481caffb2baeb7acd5e6
lucile bessac <lucilebessac@free.fr>
2023-12-03 23:17:57 +0100

    Lucile : Modif chemins itrameur

--- programmes/itrameur_dump.sh
+++ programmes/itrameur_dump.sh
@@ -2,22 +2,22 @@ langue=$1
 compteur=1
 
 
-echo "<lang=$langue>" > ../itrameur/dumps/${langue}/dumps.txt
+echo "<lang=$langue>" > ../itrameur/dumps-text-${langue}.txt
 
 
-for file in ../extractions/${langue}/* ;
+for file in ../dumps-text/dump-text-${langue}* ;
 do
 
-dump=$(cat $file | sed -e "s/>/\@lg\;/g" -e "s/</\@lt\;/g" -e "s/&/\@amp\;/g")
+dump=$(cat $file | sed -e "s/>/\@gt\;/g" -e "s/</\@lt\;/g" -e "s/&/\@amp\;/g")
 
 echo "<page='extraction$compteur'>
     <text>
     $dump
     </text>
-    </page>" > test3.txt >> ../itrameur/dumps/${langue}/dumps.txt
+    </page>" > test3.txt >> ../itrameur/dumps-text-${langue}.txt
 
     ((compteur++))
 
 done
 
-echo "</lang>" >> ../itrameur/dumps/${langue}/dumps.txt
+echo "</lang>" >> ../itrameur/dumps-text-${langue}.txt
