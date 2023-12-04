5930c43d044266b3b245286bfc48690e96ffac5e
Florian Philippe <florian.philippegm@gmail.com>
2023-12-04 15:19:51 +0100

    Mise à jour du script anglais avec ajout du concordancier

---
+++ programmes/concordancier.sh
@@ -0,0 +1,29 @@
+file=$1
+langue=$2
+compteur=$3
+echo "<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset='UTF-8'>
+            <link rel=stylesheet href=../Css/main.css>
+        </head>" > ../Concordances/concordances-${langue}-${compteur}.html
+echo "      <body>
+                <div class='container is-max-desktop'>
+                <table id=table>
+                    <tr>
+                    <th> contexte gauche </th>
+                    <th> mot </th>
+                    <th> contexte droit </th>
+                    </tr>" >> ../Concordances/concordances-${langue}-${compteur}.html
+        
+while read  line;do
+    # "$line" entre guillemets pour garder les tabs. Sinon les tabs sont interprétés comme des espaces et son collapsed.
+    contexte_gauche=$(echo "$line" | cut -f 1)
+    contexte_droit=$(echo "$line" | cut -f 3)
+    mot=$(echo "$line" | cut -f 2)
+    echo "              <tr>
+                        <td>${contexte_gauche}</td>
+                        <td>${mot}</td>
+                        <td>${contexte_droit}</td>
+                        </tr> " >> ../Concordances/concordances-${langue}-${compteur}.html
+done < $file
