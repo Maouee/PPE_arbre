80aadaa5e65190aabf4b579cc4c54987dffb4867
lucile bessac <lucilebessac@free.fr>
2024-01-06 13:15:27 +0100

    Lucile : nuage>

---
+++ nuages/nuage_russe.sh
@@ -0,0 +1,1 @@
+wordcloud_cli --text ../itrameur/contextes-russe.txt --image image.png --scale 5 --background white --contour_width 5 --mask tree.png --stopwords stop_words_russe.txt --max_words 120
